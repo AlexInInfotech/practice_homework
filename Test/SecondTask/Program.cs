f09a9e14bf796d5288c10e3bf6d45e462753f9fb
Alex <128640155+AlexPomelo@users.noreply.github.com>
2023-08-24 22:16:29 +0300

    Add files via upload

---
+++ Test/SecondTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+internal class Program
+{
+    public static string Encode(string str)
+    {
+        int code;
+        string newstroke = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            code = (int)str[i] + 2;
+            newstroke += (char)code;
+        }
+        return newstroke.ToString();
+    }
+
+    public static string Decode(string str)
+    {
+        int code;
+        string newstroke = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            code = (int)str[i] - 2;
+            newstroke += (char)code;
+        }
+        return newstroke.ToString();
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите строку: ");
+        string stroke = Console.ReadLine();
+        stroke = Encode(stroke);
+        Console.WriteLine("Закодированная строка: " + stroke);
+        stroke = Decode(stroke);
+        Console.WriteLine("Раскодированная строка: " + stroke);
+    }
+}
---
+++ Test/Test/FirstTask.cs
@@ -0,0 +1,45 @@
+﻿internal class FirstTask
+{
+    
+    private static void Main(string[] args)
+    {   
+        List<int> numbers = new List<int>();
+
+        Console.Write("Сколько чисел вы хотите ввести: ");
+        int.TryParse(Console.ReadLine(), out int length);
+        int temp;
+        int RealNumLength = length;
+        Console.WriteLine("Вводите: ");
+
+        for (int i = 0; i < length; i++)
+        {
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                numbers.Add(num);
+            }
+            else
+            {
+                RealNumLength--;
+            }
+        }
+        for (int j = 1; j < RealNumLength; j++)
+        {
+            for (int i = 0; i < RealNumLength - j; i++)
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    temp = numbers[i + 1];
+                    numbers[i + 1] = numbers[i];
+                    numbers[i] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Отсортированный список: ");
+
+        for (int i = 0; i < RealNumLength; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
